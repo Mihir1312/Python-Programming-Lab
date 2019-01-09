c122fbea8ecf1a8f2bcf4be1fc6919256c1da1ff
Mihir1312 <46518815+Mihir1312@users.noreply.github.com>
2019-01-09 13:49:29 +0530

    Add files via upload

---
+++ div1.py
@@ -0,0 +1,6 @@
+r=int(input('enter radius of circle '))
+A = 3.14*r*r 
+print('area of circle is ')
+print(A)
+
+
---
+++ sum1.py
@@ -0,0 +1,7 @@
+x=int(input('enter first number '))
+y=int(input('enter second number '))
+z=x+y
+print('sum of two numbers is ')
+print(z)
+
+
